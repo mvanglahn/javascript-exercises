477cd7028d4d022c31c2bd72f330616b1ed5da9e
Michael Van Glahn <michael.vanglahn.jr@gmail.com>
2022-08-17 13:06:05 -0400

    Passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split("").reverse().join("");
 };
 
+
+reverseString('test');
 // Do not edit below this line
 module.exports = reverseString;
