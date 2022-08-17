5a82609978d653d0621e38162bd2c5489bdda486
Michael Van Glahn <michael.vanglahn.jr@gmail.com>
2022-08-17 12:44:30 -0400

    Passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
 
+const repeatString = function(s, x) {
+    let rptStr = '';
+    if (x >= 0){
+        
+        for (let i=0; i<x; i++) {
+            rptStr = rptStr + s;
+            }
+    }   else {
+            rptStr = 'ERROR';
+        }
+    return(rptStr);
 };
 
+//repeatString('hey',3);
 // Do not edit below this line
 module.exports = repeatString;
