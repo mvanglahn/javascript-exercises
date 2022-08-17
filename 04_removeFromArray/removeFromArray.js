1cd12c027acbcd55cce58cf5e9dafcdbd7fb66a1
Michael Van Glahn <michael.vanglahn.jr@gmail.com>
2022-08-17 18:23:52 -0400

    passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...arr) {
+    const args = Array.prototype.slice.call(arr);
+    let newArr = args[0];
+    let rem = args.slice(1);
+//    console.log(newArr, rem);
+    let final = newArr.filter(a => !rem.includes(a));
+    return(final);
 };
 
+//removeFromArray([1,2,3,4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
