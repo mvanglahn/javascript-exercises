f5a63c72244438b13a49a78daf9fec6bb1c0eecd
Michael Van Glahn <michael.vanglahn.jr@gmail.com>
2022-08-17 20:52:41 -0400

    All passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  cel = (far - 32)* 5/9;
+  var celOut = Math.round(cel * 10) / 10
+  console.log(celOut);
+  return celOut;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  far = cel * 9/5 + 32;
+  var farOut = Math.round(far * 10) / 10
+  console.log(farOut);
+  return(farOut);
 };
 
+ftoc(32);
+ctof(0);
 // Do not edit below this line
 module.exports = {
   ftoc,
