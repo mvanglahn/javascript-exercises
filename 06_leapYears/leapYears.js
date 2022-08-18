bd060c22e8e77054aeb0bd52633d15df5e7f575f
Michael Van Glahn <michael.vanglahn.jr@gmail.com>
2022-08-17 20:46:18 -0400

    All passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 400 == 0){
+        console.log('true')
+        return true;
+    }
+    else if (year % 100 == 0){
+        console.log('false')
+        return false;
+    } 
+    else if (year % 4 == 0){
+        console.log('true');
+        return true;
+    }
+    else {
+        console.log('false')
+        return false;
+    }
+}; 
 
-};
-
+leapYears(34992);
 // Do not edit below this line
 module.exports = leapYears;
